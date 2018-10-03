96779841b3800fdb260f20217cc796c07858609b
Alexandra Havia <sasha.havia@gmail.com>
2018-10-03 16:09:12 -0400

    Client (store): Created reducer in a seperate file

--- client/store/index.js
+++ client/store/index.js
@@ -1,14 +1,13 @@
-import {createStore, combineReducers, applyMiddleware} from 'redux'
+import {createStore, applyMiddleware} from 'redux'
 import createLogger from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
-import user from './user'
+import rootReducer from './reducer'
 
-const reducer = combineReducers({user})
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
 )
-const store = createStore(reducer, middleware)
+const store = createStore(rootReducer, middleware)
 
 export default store
 export * from './user'
--- client/store/user.js
+++ client/store/user.js
@@ -8,15 +8,15 @@ const GET_USER = 'GET_USER'
 const REMOVE_USER = 'REMOVE_USER'
 
 /**
- * INITIAL STATE
- */
-const defaultUser = {}
-
-/**
  * ACTION CREATORS
  */
-const getUser = user => ({type: GET_USER, user})
-const removeUser = () => ({type: REMOVE_USER})
+const getUser = user => ({
+  type: GET_USER,
+  user
+})
+const removeUser = () => ({
+  type: REMOVE_USER
+})
 
 /**
  * THUNK CREATORS
@@ -24,7 +24,7 @@ const removeUser = () => ({type: REMOVE_USER})
 export const me = () => async dispatch => {
   try {
     const res = await axios.get('/auth/me')
-    dispatch(getUser(res.data || defaultUser))
+    dispatch(getUser(res.data || {}))
   } catch (err) {
     console.error(err)
   }
@@ -59,13 +59,15 @@ export const logout = () => async dispatch => {
 /**
  * REDUCER
  */
-export default function(state = defaultUser, action) {
+const userReducer = (state = {}, action) => {
   switch (action.type) {
     case GET_USER:
       return action.user
     case REMOVE_USER:
-      return defaultUser
+      return {}
     default:
       return state
   }
 }
+
+export default userReducer
